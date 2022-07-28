04a037d4b6652cf904e29cf2e777cdb031762e78
abanwasi <abanwasi@sovrn.com>
2022-07-28 07:51:17 -0600

    Added back the code thtaw as deleted by mistake

--- pygithubonerpub.py
+++ pygithubonerpub.py
@@ -2,41 +2,99 @@ import requests
 import json
 import pandas as pd
 import datetime as dt
+from termcolor import colored
 
 
 ####
-# inputs
+#  Inputs
+#  username: username
+#  token : token
+#  orgname : by default this will be sovrn. Can be specified as the user's github name 
+#  repos: List of repos to check
+#  closedate: Will check for pull requests after this specific date and all other open pull requests that haven't yet been closed
 ####
-username = 'sovrnabanwasi'
 
-# from https://github.com/user/settings/tokens
-token = ''
-token = 'xxxx'
-repo = 'xxx'
 
-repos_url = 'https://api.github.com/orgs/sovrn/repos?per_page=100&page=1'
-pull_url = 'https://api.github.com/repos/sovrn/{reponame}/pulls?state=all'
-# create a re-usable session object with the user creds in-built
-gh_session = requests.Session()
-gh_session.auth = (username, token)
-newurl = pull_url.replace('{reponame}',repo)
-pulls = json.loads(gh_session.get(newurl).text)
 
-newpulls = list(filter(lambda d: d['created_at'] >= '2022-07-01', pulls))
 
-if len(newpulls) > 0:
-    for item in newpulls:
-        merged = item['merged_at']
-        closed = item['closed_at']
-        if not merged:
-            print(closed)
-            merged = dt.datetime.today().strftime("%Y-%m-%d")
+def main():
+    pd.options.display.max_colwidth = 1000
+    
+    pull_url = 'https://api.github.com/repos/{orgname}/{reponame}/pulls?state=all'
+    username = 'sovrnabanwasi'
+    orgname = 'sovrn'
+    token = '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'
+    repos = ['testrepoforpr', 'viglink']
+    pulldate = '2022-07-01'
+
+    # from https://github.com/user/settings/tokens
+    # repos_url = 'https://api.github.com/orgs/sovrn/repos?per_page=100&page=1'
+ 
+    
+    # create a re-usable session object with the user creds in-built
+    gh_session = requests.Session()
+    gh_session.auth = (username, token)
+    newurl = pull_url.replace('{orgname}',orgname)
+    
+    pull_req_list = []
+    for reponame in repos:
+        pull_url = newurl.replace('{reponame}',reponame)
+        
+        resp = gh_session.get(pull_url)
+        if resp.status_code == 200:
+            pulls = json.loads(resp.text)
+            newpulls = []
+            newpulls = list(filter(lambda d: d['created_at'] >= pulldate, pulls))
+
+            if len(newpulls) > 0:
+                for item in newpulls:
+                    pull_req_dict = {}
+                    ##print(item.keys())
+                    merged = item['merged_at']
+                    closed = item['closed_at']
+                    status = 'merged'
+                    if not merged:
+                        merged = dt.datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ")
+                        status = 'open'
+                    else:
+                        merged = merged
+                    created = (item['created_at'])
+                    #created = created[:10]
+                    #print (reponame + ": Pull Request Title::::" + item['title'])
+                    merge_time = dt.datetime.strptime(merged, "%Y-%m-%dT%H:%M:%SZ") -dt.datetime.strptime(created, "%Y-%m-%dT%H:%M:%SZ")
+                    pull_req_dict['reponame'] = reponame
+                    pull_req_dict['status'] = status
+                    pull_req_dict['title'] = item['title']
+                    pull_req_dict['merge_hours'] = merge_time.total_seconds()/3600
+                    pull_req_list.append(pull_req_dict)
+                    #if status == 'open':
+                        #print("Open Pull Requests")
+                        #print("PR Open Time:" + str(merge_time))
+                    #else:
+                        #print("Closed Pull Requests")
+                        #print("PR Merge Time:" + str(merge_time))
+                    
         else:
-            merged = merged[:10]
-        created = (item['created_at'])
-        created = created[:10]
-        print (repo + ": Pull Request Title::::" + item['title'])
-        merge_time = dt.datetime.strptime(merged, "%Y-%m-%d") -dt.datetime.strptime(created, "%Y-%m-%d")
-        print("PR Merge Time:" + str(merge_time))
+            pull_req_dict = {}
+            pull_req_dict['reponame'] = reponame
+            pull_req_dict['status'] = 'doesnotexist'
+            pull_req_dict['title'] = ''
+            pull_req_dict['merge_hours'] = 0
+            pull_req_list.append(pull_req_dict)
+    
+    df = pd.DataFrame(pull_req_list)
 
+    df_green  = pd.DataFrame()
+    df_yellow  = pd.DataFrame()
+    df_red  = pd.DataFrame()
 
+    df_green  = df[df['merge_hours'] < 48]
+    df_red  = df[df['merge_hours'] > 72 ]
+    df_yellow  =  df[(df['merge_hours'] >= 99) & (df['merge_hours'] <= 101)]
+
+    print(colored(df_green, 'green'))
+    print(colored(df_yellow, 'yellow'))
+    print(colored(df_red, 'red'))
+    
+    
+main()
